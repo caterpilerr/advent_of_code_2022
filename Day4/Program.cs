61fa7f064927b25e3bc2b95079737bed41acc06e
Viktor Belokashin <viktor_belokashin@epam.com>
2022-12-05 21:07:09 +0600

    add day 4 and 5

---
+++ Day4/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+const string FilePath = "input.txt";
+
+var input = File.ReadAllLines(FilePath);
+var data = new int[input.Length][];
+for (var i = 0; i < input.Length; i++)
+{
+    var elvesPair = input[i].Split(',');
+    data[i] = elvesPair.SelectMany(
+        elfData => elfData.Split('-').Select(symbol => int.Parse(symbol))
+    ).ToArray();
+}
+
+var invalidPairs = 0;
+var overlappingPairs = 0;
+foreach (var item in data)
+{
+    // Part 1
+    var startDelta = item[0] - item[2];
+    var endDelta = item[1] - item[3];
+    if (startDelta == 0 ||
+        endDelta == 0 ||
+        startDelta > 0 == endDelta < 0)
+    {
+        invalidPairs++;
+    }
+
+    // Part 2
+    var firstDelta = item[2] - item[1];
+    var secondDelta = item[0] - item[3];
+    if (firstDelta > 0 && secondDelta < 0 ||
+        firstDelta < 0 && secondDelta > 0)
+    {
+        continue;
+    }
+
+    overlappingPairs++;
+}
+
+Console.WriteLine($"The number of invalid elves pairs: {invalidPairs}");
+Console.WriteLine($"The number of ovelapping elves pairs: {overlappingPairs}");
+
---
+++ Day5/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+const string FilePath = "input.txt";
+
+var input = File.ReadAllLines(FilePath);
+
+var stacks = new Stack<char>[]
+{
+    new Stack<char> (new [] {'W', 'B', 'D', 'N', 'C', 'F', 'J'}),
+    new Stack<char> (new [] {'P', 'Z', 'V', 'Q', 'L', 'S', 'T'}),
+    new Stack<char> (new [] {'P', 'Z', 'B', 'G', 'J', 'T'}),
+    new Stack<char> (new [] {'D', 'T', 'L', 'J', 'Z', 'B', 'H', 'C'}),
+    new Stack<char> (new [] {'G', 'V', 'B', 'J', 'S'}),
+    new Stack<char> (new [] {'P', 'S', 'Q'}),
+    new Stack<char> (new [] {'B', 'V', 'D', 'F', 'L', 'M', 'P', 'N'}),
+    new Stack<char> (new [] {'P', 'S', 'M', 'F', 'B', 'D', 'L', 'R'}),
+    new Stack<char> (new [] {'V', 'D', 'T', 'R'}),
+};
+
+var data = input.Select(
+    line => line.Split(' ')
+    .Where((val, index) => index % 2 != 0)
+    .Select(x => int.Parse(x))
+    .ToArray());
+
+var tempStack = new Stack<char>();
+foreach(var command in data)
+{
+    var amount = command[0];
+    var from = command[1] - 1;
+    var to = command[2] - 1;
+    for (var i = 0; i < amount; i++)
+    {
+        tempStack.Push(stacks[from].Pop());
+    }
+
+    for (var i = 0; i < amount; i++)
+    {
+        stacks[to].Push(tempStack.Pop());
+    }
+}
+
+Array.ForEach(stacks, stack => Console.Write(stack.Peek()));
