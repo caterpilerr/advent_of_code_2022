df42ba76a2c218a11182b96d482d1e130d3f0912
Viktor Belokashin <viktor_belokashin@epam.com>
2022-12-07 21:55:10 +0600

    add day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+const string FilePath = "input.txt";
+const int PacketSyncLength = 4;
+const int MessageSyncLength = 14;
+
+var input = File.ReadAllLines(FilePath);
+
+foreach (var line in input)
+{
+    var packetStart = FindMarker(line, 0, PacketSyncLength);
+    var messageStart = FindMarker(line, packetStart, MessageSyncLength);
+    Console.WriteLine($"Number of packet index: {packetStart}");
+    Console.WriteLine($"Number of message index: {messageStart}");
+}
+
+int FindMarker(string packet, int offset, int syncLength)
+{
+    var symbols = new int[26];
+    var slow = offset;
+    var fast = offset;
+    while (fast < packet.Length)
+    {
+        var fastCharOffset = packet[fast] - 'a';
+        symbols[fastCharOffset]++;
+        if (fast - slow + 1 == syncLength)
+        {
+            if (IsWithoutDuplicates(symbols))
+            {
+                return fast + 1;
+            }
+
+            var slowCharOffset = packet[slow] - 'a';
+            symbols[slowCharOffset]--;
+            slow++;
+        }
+
+        fast++;
+    }
+
+    return -1;
+}
+
+bool IsWithoutDuplicates(int[] charData)
+{
+    foreach (var count in charData)
+    {
+        if (count > 1)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
